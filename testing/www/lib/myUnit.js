51f20df2b9d57384fb397abfcca544133f2243e3
Rebecca Murphey <rmurphey@gmail.com>
2012-11-01 09:48:13 -0400

    simpler myUnit.js

--- testing/www/lib/myUnit.js
+++ testing/www/lib/myUnit.js
@@ -1,24 +1,16 @@
 window.test = function( msg, fn ) {
-  var fail, assertion, el;
+  var fail, el;
 
   try {
     fn();
   } catch( e ) {
-    fail = e.message;
+    fail = true;
   }
 
   el = document.createElement( 'p' );
   el.innerHTML = ( fail ? 'FAIL: ' : 'PASS: ' ) + msg;
   el.className = fail ? 'fail' : 'pass';
   document.body.appendChild( el );
-
-  if ( fail ) {
-    assertion = document.createElement( 'p' );
-    assertion.innerHTML = fail;
-    assertion.className = ( fail ? 'fail' : 'pass' ) + ' assertion';
-
-    document.body.appendChild( assertion );
-  }
 };
 
 window.assert = function( val, msg ) {
