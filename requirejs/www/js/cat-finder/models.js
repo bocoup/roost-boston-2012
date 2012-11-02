7414dad9e93f667b1df77378379d9fe11f4ef0fa
Rebecca Murphey <rmurphey@gmail.com>
2012-11-02 19:28:40 -0400

    simplifying

--- requirejs/www/js/cat-finder/models.js
+++ requirejs/www/js/cat-finder/models.js
@@ -1,28 +1,22 @@
 window.CatFinder = window.CatFinder || {};
 
 CatFinder.Models = {};
-CatFinder.Collections = {};
-
-CatFinder.Models.Cat = Backbone.Model.extend();
-
-CatFinder.Collections.Cats = Backbone.Collection.extend({
-  model: CatFinder.Models.Cat,
-
-  url: 'js/cat-finder/cats.json'
-});
 
 CatFinder.Models.App = Backbone.Model.extend({
   initialize: function() {
     this.on('change:searchTerm', function(term) {
+
       var matches = CatFinder.cats.filter(function(cat) {
         return cat.get('name').match(term);
       });
+
       this.set('results', matches);
+
     }, this);
   }
 });
 
-CatFinder.cats = new CatFinder.Collections.Cats([
+CatFinder.cats = new Backbone.Collection([
   { name: 'Myster', type: 'black' },
   { name: 'Sap', type: 'tortoise' },
   { name: 'Max', type: 'siamese' }
