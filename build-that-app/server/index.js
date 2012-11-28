5a62a6529f93cbce06bcd3956c486a12d1e4fcb5
Rebecca Murphey <rmurphey@gmail.com>
2012-11-28 12:32:05 -0500

    updating server to serve tests & fake data

--- build-that-app/server/index.js
+++ build-that-app/server/index.js
@@ -1,39 +1,50 @@
 var express   = require( 'express' );
-var Faker     = require( 'Faker' );
 var _         = require( 'underscore' )
 var app       = express( express.bodyParser() );
 var baseDir   = __dirname + '/../';
+var sensors   = [];
 
-var fakeData = [];
+var Sensor = function( id ) {
+  this.id = id;
+  this.data = [];
+};
 
-for (var i = 0; i < 1000; i++) {
-  fakeData.push(Faker.Helpers.userCard());
+Sensor.prototype.generateData = function() {
+  this.data.push( Math.random() );
+  this._cleanup();
+};
+
+Sensor.prototype._cleanup = function() {
+  if ( this.data.length > 1000 ) {
+    this.data.shift();
+  }
+};
+
+for ( var i = 0; i < 5; i++ ) {
+  var s = new Sensor( 'sensor-' + i );
+  sensors.push( s );
+
+  (function(s){
+    setTimeout(function() {
+      s.generateData();
+    }, 1000);
+  }(s));
 }
 
 app.use( '/', express.static( baseDir + 'public' ) );
+app.use( '/test', express.static( baseDir + 'test') );
 
-app.get( '/data/search.json', function(req, res) {
-  var query = req.query.q;
-  var results = [];
-
-  if (!query || !query.trim()) {
-    console.log('Query was empty.');
-  } else {
-    results = _.filter(fakeData, function(item) {
-      var possibles = [ item.name, item.email, item.company.name ];
-      return _.any(possibles, function(p) {
-        return p.toLowerCase().match(query.toLowerCase());
-      });
-    });
-  }
-
-  // simulate latency
-  setTimeout(function() {
-    res.end(JSON.stringify({ results : results }));
-  }, 500);
+app.get( '/data/sensors', function( req, res ) {
+  res.json({ sensors: sensors });
 });
 
-app.use( '/test', express.static( baseDir + 'test') );
+app.get( '/data/sensors/:sensorId', function( req, res ) {
+  var s = _.filter( sensors, function(s) {
+    return s.id === req.params.sensorId;
+  });
+
+  res.json( s );
+});
 
 app.listen( 4000 );
 
