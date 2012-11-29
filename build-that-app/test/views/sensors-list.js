74f630dbfc8eef52358ebb0d89043f17591dd944
Rebecca Murphey <rmurphey@gmail.com>
2012-11-29 13:23:13 -0500

    better tests for add/change/reset events

--- build-that-app/test/views/sensors-list.js
+++ build-that-app/test/views/sensors-list.js
@@ -6,15 +6,15 @@ define([
   'jquery'
 ], function( SensorsList, B, $ ) {
   suite('SensorsList', function() {
-    var sensorCollection;
-
-    var sensors = [
-      { id: 'sensor-1', name: 'sensor 1', data: [ 1, 2, 3 ] },
-      { id: 'sensor-2', name: 'sensor 2', data: [ 1, 2, 3 ] },
-      { id: 'sensor-3', name: 'sensor 3', data: [ 1, 2, 3 ] }
-    ];
+    var sensorCollection, sensors;
 
     setup(function() {
+      sensors = [
+        { id: 'sensor-1', name: 'sensor 1', data: [ 1, 2, 3 ], type: 'fake' },
+        { id: 'sensor-2', name: 'sensor 2', data: [ 1, 2, 3 ], type: 'fake' },
+        { id: 'sensor-3', name: 'sensor 3', data: [ 1, 2, 3 ], type: 'fake' }
+      ];
+
       $('#test').remove();
       $('<div id="test">').appendTo('body');
       sensorCollection = new B.Collection( sensors );
@@ -49,7 +49,7 @@ define([
       sl.destroy();
     });
 
-    test('Render updated data', function() {
+    test('Render on new data', function() {
       var sl = new SensorsList({
         collection: sensorCollection,
         el: '#test'
@@ -68,21 +68,53 @@ define([
       sl.destroy();
     });
 
-    test('Render on collection reset', function() {
-      var flag = false;
-
+    test('Render on data reset', function() {
       var sl = new SensorsList({
         collection: sensorCollection,
         el: '#test'
       });
 
-      sl.postRender = function() {
-        flag = true;
-      };
+      sl.render();
 
-      sensorCollection.trigger('reset');
-      assert( flag );
+      assert.equal( $('#test li').length, 3 );
+
+      sensors.push({ id: 'sensor-4', name: 'sensor 4', data: [ 1, 2, 3 ], type: 'fake' });
+
+      sensorCollection.reset( sensors )
+
+      assert.equal( $('#test li').length, 4 );
     });
 
+    test('Render on data add', function() {
+      var sl = new SensorsList({
+        collection: sensorCollection,
+        el: '#test'
+      });
+
+      sl.render();
+
+      assert.equal( $('#test li').length, 3 );
+
+      sensorCollection.add({ id: 'sensor-4', name: 'sensor 4', data: [ 1, 2, 3 ], type: 'fake' });
+
+      assert.equal( $('#test li').length, 4 );
+    });
+
+    test('Render on data change', function() {
+      var sl = new SensorsList({
+        collection: sensorCollection,
+        el: '#test'
+      });
+
+      sl.render();
+
+      assert.equal( $('#test li').length, 3 );
+
+      var model = sensorCollection.get('sensor-3');
+      model.set('name', 'renamed sensor');
+
+      assert.equal( $('#test li').length, 3 );
+      assert( $('#test').html().match('renamed sensor') );
+    });
   });
 });
